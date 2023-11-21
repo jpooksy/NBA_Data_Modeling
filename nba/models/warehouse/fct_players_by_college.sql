e8d7eef7d8c17558ea4c191bed1ad6c7484bebc7
Parker Rogers <parker@paradime.io>
2023-11-21 19:31:08 +0000

    updated model

--- nba/models/warehouse/fct_players_by_college.sql
+++ nba/models/warehouse/fct_players_by_college.sql
@@ -1,7 +1,7 @@
 with players_by_college as (
     select 
         school,
-        count(*) as player_count_totals
+        count(*) as player_count
     from 
         {{ ref('source_common_player_info') }}
     where
