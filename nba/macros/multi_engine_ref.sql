f130e9f5947981c10a708ff0bb070a6df1b8d593
Parker Rogers <parker@paradime.io>
2023-12-19 19:30:21 +0000

    cast macro to uppercase

--- nba/macros/multi_engine_ref.sql
+++ nba/macros/multi_engine_ref.sql
@@ -1,14 +1,12 @@
 {% macro multi_engine_ref(table_name, source_name=none) %}
---  Snowflake Target
-    {% if target.name == 'dev' %}
-        {{ source(source_name.upper(), table_name.upper()) }}
-        --  add jinja function to change lowercase to uppercase
---  duckdb targets
+    -- Snowflake Target
+    {% if target.name == 'dev' or target.name == 'prod' %}
+        {{ source(source_name | upper, table_name | upper) }}
+    -- duckdb targets
     {% elif target.name in ['dev_mother_duck', 'dev_duck_db'] %}
         {{ ref(table_name) }}
     {% else %}
         -- Handle other targets or provide a default action
         SELECT 'Unsupported target' as error_message
     {% endif %}
-
 {% endmacro %}
